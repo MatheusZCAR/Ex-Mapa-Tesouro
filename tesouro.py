1ed314eeea37dac2240ddca7b7f7792924ceadde
“Joao <joaotapparo@protonmail.com>
2025-02-12 11:36:16 -0300

    first commit

--- tesouro.py
+++ tesouro.py
@@ -1,1 +1,35 @@
-print("Olá")
+import numpy as np
+
+matrix = np.array([
+    ["#", "#", "#", "#", "#", "#", "#"],
+    ["#", "S", ".", ".", ".", ".", "#"],
+    ["#", ".", "#", ".", "#", "#", "#"],
+    ["#", ".", ".", ".", "#", ".", "#"],
+    ["#", ".", "#", "#", "#", ".", "#"],
+    ["#", ".", ".", "T", ".", ".", "#"],
+    ["#", "#", "#", "#", "#", "#", "#"]
+])
+
+
+baixo = 1
+direita = 1
+verificador = 0
+
+
+while matrix[baixo][direita]:
+    if matrix[baixo][direita] == "T":
+        print("Tesouro encontrado!")
+        break
+    elif matrix[baixo][direita] == "#":
+        if(verificador == 0):
+            print("Parede encontrada!")
+            baixo -= 1
+            direita += 1
+            verificador = 1
+        else:
+            baixo += 1
+            direita -= 1
+            verificador = 0
+    else:
+        print("Caminhando...")
+        baixo += 1
