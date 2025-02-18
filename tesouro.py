a191a5f2dd7dbfeeae1cbdf232de3173a2b2f9ef
Matheus Zanon Caritá <107090798+MatheusZCAR@users.noreply.github.com>
2025-02-18 11:31:39 -0300

    Adicionando comentários no código

--- tesouro.py
+++ tesouro.py
@@ -1,10 +1,10 @@
-import numpy as np
-import random as ran
+import numpy as np   # biblioteca para manipulação de matrizes
+import random as ran  # biblioteca para escolhas aleatórias
 
-linha = 1
-coluna = 1
-opcoes = [1, 2, 3, 4]
-passos = 0
+linha = 1  # linha da matriz, somar 1 fará com que o personagem vá para baixo, subtrair 1 fará com que o personagem S vá para cima
+coluna = 1 # coluna da matriz, somar 1 fará com que o personagem vá para direita, subtrair 1 fará com que o personagem S vá para esquerda
+opcoes = [1, 2, 3, 4]   # possibilidades de movimentos
+passos = 0  # número de passos dados
 
 matrix = np.array([
     ["#", "#", "#", "#", "#", "#", "#"],
@@ -14,21 +14,21 @@ matrix = np.array([
     ["#", ".", "#", "#", "#", ".", "#"],
     ["#", ".", ".", "T", ".", ".", "#"],
     ["#", "#", "#", "#", "#", "#", "#"]
-])
+])  # a matriz representa o mapa do tesouro, sendo # caminho bloqueado, . caminho livre, S o jogador e T o tesouro
 
-def andar_baixo():
-    global linha, coluna, opcoes, passos, matrix
-    if matrix[linha + 1][coluna] != "#":
-        matrix[linha][coluna] = "."
-        linha += 1
-        passos += 1
+def andar_baixo():   # função de andar para baixo
+    global linha, coluna, opcoes, passos, matrix   # tornando as variáveis globais para que possam ser usadas dentro da função
+    if matrix[linha + 1][coluna] != "#":   # se o caminho de baixo não estiver bloqueado
+        matrix[linha][coluna] = "."   # define o caminho atual como livre
+        linha += 1   # vai para baixo
+        passos += 1  # soma 1 passo
         
-        if matrix[linha][coluna] == "T":
+        if matrix[linha][coluna] == "T":  # se o caminho que fomos é o T, achamos o tesouro
             print(f"Tesouro encontrado em {passos} passos")
             return
-        else:
+        else:   # caso contrário, só andamos para baixo
             print("Andando para baixo")
-            matrix[linha][coluna] = "S"
+            matrix[linha][coluna] = "S"   # definimos a nova posição como a do jogador
             return
 
 def andar_cima():
@@ -76,19 +76,19 @@ def andar_esquerda():
             matrix[linha][coluna] = "S"
             return
     
-def main():
+def main():   # função principal
     global linha, coluna, opcoes, passos, matrix
-    while matrix[linha][coluna] != "T":
-        escolhido = ran.choice(opcoes)
+    while matrix[linha][coluna] != "T":   # enquanto não chegarmos ao tesouro, executaremos o código
+        escolhido = ran.choice(opcoes)   # escolhido é uma variável que recebe a escolha aleatória das opcoes
         
-        if escolhido == 1:
+        if escolhido == 1:  # se a opção for 1, ande para baixo
             andar_baixo()
-        if escolhido == 2:
+        if escolhido == 2:  # se a opção for 2, ande para cima
             andar_cima()
-        if escolhido == 3:
+        if escolhido == 3:  # se a opção for 3, ande para direita
             andar_direita()
-        if escolhido == 4:
+        if escolhido == 4:  # se a opção for 4, ande para esquerda
             andar_esquerda()
             
-if __name__ == "__main__":
+if __name__ == "__main__":   # para iniciar na main
     main()
