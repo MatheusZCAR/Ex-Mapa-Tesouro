65fa5a22a9af71e296f89d54a1742e45a294a492
Matheus Zanon Caritá <107090798+MatheusZCAR@users.noreply.github.com>
2025-02-18 11:23:36 -0300

    Atualizando o algoritmo
    implementado o algoritmo de procura aleatória (sem interface gráfica)

--- tesouro.py
+++ tesouro.py
@@ -1,7 +1,9 @@
 import numpy as np
+import random as ran
 
-linha = 1   # tem que deixar a variável global
+linha = 1
 coluna = 1
+opcoes = [1, 2, 3, 4]
 passos = 0
 
 matrix = np.array([
@@ -14,118 +16,79 @@ matrix = np.array([
     ["#", "#", "#", "#", "#", "#", "#"]
 ])
 
-
-def andar_pra_baixo():
-    matrix[linha][coluna] = "."   # substitui a casa atual onde S está por uma casa vaga
-    linha += 1
-    print("Caminhou uma casa pra baixo")
-    passos += 1
-    print(f"Total de passos: {passos}")
-    if matrix[linha][coluna] == "T":      # se a casa vaga na verdade for o Tesouro, avisar que o tesouro foi encontrado
-            print("Tesouro encontrado!")
-            return("T")
-    else:
-        matrix[linha][coluna] = "S"    # move S para a casa que estava vaga antes
-    return
-
-def andar_pra_cima():
-    matrix[linha][coluna] = "."
-    linha -= 1
-    print("Caminhou uma casa pra cima")
-    passos += 1
-    print(f"Total de passos: {passos}")
-    if matrix[linha][coluna] == "T":
-            print("Tesouro encontrado!")
-            return("T")
-    else:
-        matrix[linha][coluna] = "S"
-    return
-
-def andar_pra_direita():
-    matrix[linha][coluna] = "."
-    coluna += 1
-    print("Caminhou uma casa pra direita")
-    passos += 1
-    print(f"Total de passos: {passos}")
-    if matrix[linha][coluna] == "T":
-            print("Tesouro encontrado!")
-            return("T")
-    else:
-        matrix[linha][coluna] = "S"
-    return
-
-def andar_pra_esquerda():
-    matrix[linha][coluna] = "."
-    coluna -= 1
-    print("Caminhou uma casa pra esquerda")
-    passos += 1
-    print(f"Total de passos: {passos}")
-    if matrix[linha][coluna] == "T":
-            print("Tesouro encontrado!")
-            return("T")
-    else:
-        matrix[linha][coluna] = "S"
-    return
-
-
-
-
-'''
-    while matrix[linha][coluna] != "T":
-    
-    if matrix[linha + 1][coluna] == "." or matrix[linha + 1][coluna] == "T":  # andar pra baixo
-        andar_pra_baixo()
-    
-    if matrix[linha - 1][coluna] == "." or matrix[linha - 1][coluna] == "T": # andar pra cima
-        
-        if matrix[linha + 1][coluna] == "." or matrix[linha + 1][coluna] == "T":    # verifica se dá pra andar mais pra baixo
-            andar_pra_baixo()
-        
-        else:
-            
-        
-    
-    elif matrix[linha][coluna + 1] == "." or matrix[linha][coluna + 1] == "T":
-        coluna += 1
-        print("Caminhou uma casa para direita")
+def andar_baixo():
+    global linha, coluna, opcoes, passos, matrix
+    if matrix[linha + 1][coluna] != "#":
+        matrix[linha][coluna] = "."
+        linha += 1
         passos += 1
-        andouDireita = 1
-        print(f"Total de passos: {passos}")
         
         if matrix[linha][coluna] == "T":
-            print("Tesouro encontrado!") 
-    
-    elif matrix[linha][coluna - 1] == "." or matrix[linha][coluna - 1] == "T":
-        coluna -= 1
-        print("Caminhou uma casa para esquerda")
-        passos += 1
-        andouDireita = 0
-    
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-
-    if matrix[linha][coluna] == "T":
-        print("Tesouro encontrado!")
-        break
-    elif matrix[linha][coluna] == "#":
-        if(verificador == 0):
-            print("Parede encontrada!")
-            linha -= 1
-            coluna += 1
-            verificador = 1
+            print(f"Tesouro encontrado em {passos} passos")
+            return
         else:
-            linha += 1
-            coluna -= 1
-            verificador = 0
-    else:
-        print("Caminhando...")
-        linha += 1
-'''
+            print("Andando para baixo")
+            matrix[linha][coluna] = "S"
+            return
+
+def andar_cima():
+    global linha, coluna, opcoes, passos, matrix
+    if matrix[linha - 1][coluna] != "#":
+        matrix[linha][coluna] = "."
+        linha -= 1
+        passos += 1
+        
+        if matrix[linha][coluna] == "T":
+            print(f"Tesouro encontrado em {passos} passos")
+            return
+        else:
+            print("Andando para cima")
+            matrix[linha][coluna] = "S"
+            return
+
+def andar_direita():
+    global linha, coluna, opcoes, passos, matrix
+    if matrix[linha][coluna + 1] != "#":
+        matrix[linha][coluna] = "."
+        coluna += 1
+        passos += 1
+        
+        if matrix[linha][coluna] == "T":
+            print(f"Tesouro encontrado em {passos} passos")
+            return
+        else:
+            print("Andando para direita")
+            matrix[linha][coluna] = "S"
+            return
+
+def andar_esquerda():
+    global linha, coluna, opcoes, passos, matrix
+    if matrix[linha][coluna - 1] != "#":
+        matrix[linha][coluna] = "."
+        coluna -= 1
+        passos += 1
+        
+        if matrix[linha][coluna] == "T":
+            print(f"Tesouro encontrado em {passos} passos")
+            return
+        else:
+            print("Andando para esquerda")
+            matrix[linha][coluna] = "S"
+            return
+    
+def main():
+    global linha, coluna, opcoes, passos, matrix
+    while matrix[linha][coluna] != "T":
+        escolhido = ran.choice(opcoes)
+        
+        if escolhido == 1:
+            andar_baixo()
+        if escolhido == 2:
+            andar_cima()
+        if escolhido == 3:
+            andar_direita()
+        if escolhido == 4:
+            andar_esquerda()
+            
+if __name__ == "__main__":
+    main()
